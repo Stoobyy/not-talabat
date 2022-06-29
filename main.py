523336de6bb697bc635e74310eb8ec300484af51
Stooby <stoobyleany@Gmail.com>
2022-06-29 20:25:26 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+# Importing Modules
+import mysql.connector as sql
+import os
+import time
+import requests
+from cryptography.fernet import Fernet
+
+# Variables to be stored on seperate secure database
+key = 'D9QRguYyat5TWlIyfg9AFWizc91muAGD-UlpWHxT0Y8='
+# Creating required objects
+db = sql.connect(
+    host = 'localhost',
+    username = 'root',
+    password = 'stooby',
+    database = 'notTalabat'
+)
+cursor = db.cursor()
+f=Fernet(key)
+
+def login(username, password):
+    encpassword = list(cursor.execute(f'select password from userdata where username = {username}'))[0]
+    output = f.decrypt(encpassword.encode())
+    if password == output:
+        return True
